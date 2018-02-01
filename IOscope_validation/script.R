55906c96df8924e308eea03efd4de5d79f976c72
Abdulqawi Saif <abdulqawi.saif@loria.fr>
2018-02-01 10:24:17 +0100

    update R script, making labels larger

--- IOscope_validation/script.R
+++ IOscope_validation/script.R
@@ -9,7 +9,8 @@
 args <- commandArgs(trailingOnly = TRUE)
 
 library(tools)  # for the file extensions
-pdf(paste("out",file_path_sans_ext(args[1]),".pdf", sept=""),width=8, height=4)
+pdf(paste("out",file_path_sans_ext(args[1]),".pdf", sept=""), width=6, height=4.5)
+par(mai=c(1,1,0.5,.5))
 dataTable <- read.table(args[1], header=TRUE)
 ifelse("type" %in% colnames(dataTable),
 dataFrame <- data.frame(offset=dataTable$offset, latency=dataTable$latency_ms, type=dataTable$type),
@@ -19,11 +20,11 @@ temp = nrow(dataFrame)
 dataFrame$i <- seq(1,as.numeric(temp))
 if ("type" %in% colnames(dataFrame))
 {
-plot(dataFrame$i, dataFrame$offset, xlab="IO requests",ylab="Offset",  pch = 20, col = ifelse(dataFrame$type==0,'red','blue'))
-legend('topleft', legend= c("Read IO req.", "Write IO req."), col=c("red", "blue"),pch = c(19,19), cex=0.8)
+plot(dataFrame$i, dataFrame$offset, xlab="Sequences of I/O requests",ylab="File offsets",  pch = 20, col = ifelse(dataFrame$type==0,'red','blue'), cex.lab=1.8, cex.axis=1.8)
+legend('topleft', legend= c("Read IO req.", "Write IO req."), col=c("red", "blue"),pch = c(19,19), cex=1.8)
 } else {
-plot(dataFrame$i, dataFrame$offset, xlab="IO requests",ylab="Offset",  pch = 20, col ="black")
-legend('topleft', legend= c("mmap mem. faults"), col=c("black"),pch =19, cex=0.8)
+plot(dataFrame$i, dataFrame$offset, xlab="Sequences of memory faults",ylab="File offsets",  pch = 20, col ="black", cex.lab=1.8, cex.axis=1.8)
+legend('topleft', legend= c("mmap mem. faults"), col=c("black"),pch =19, cex=1.8)
 }
 invisible(dev.off())
 
